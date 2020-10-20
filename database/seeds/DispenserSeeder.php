72b5a61f4f99509d2405d1411f417676b67cb413
fauzifadillah <fauzifadillah00@yahoo.com>
2020-10-20 15:30:39 +0700

    Truncate STR

--- database/seeds/DispenserSeeder.php
+++ database/seeds/DispenserSeeder.php
@@ -13,49 +13,49 @@ class DispenserSeeder extends Seeder
     {
         DB::table('products')->insert([
             'name' => 'Cosmos CWD-1060',
-            'categories_id' => 4,
+            'categories_id' => 8,
             'price' => 60000+111111,
             'price3'=>57037
         ]);
         DB::table('products')->insert([
             'name' => 'Sanex D102',
-            'categories_id' => 4,
+            'categories_id' => 8,
             'price' => 41150+89000,
             'price3'=>43383
         ]);
         DB::table('products')->insert([
             'name' => 'Miyako WD-185 H',
-            'categories_id' => 4,
+            'categories_id' => 8,
             'price' => 60000+119500,
             'price3'=>59833
         ]);
         DB::table('products')->insert([
             'name' => 'Sanex D-188',
-            'categories_id' => 4,
+            'categories_id' => 8,
             'price' => 60000+100000,
             'price3'=>53333
         ]);
         DB::table('products')->insert([
             'name' => 'Cosmos CWD1060',
-            'categories_id' => 4,
+            'categories_id' => 8,
             'price' => 60000+111111,
             'price3'=>57037
         ]);
         DB::table('products')->insert([
             'name' => 'Miyako WD186',
-            'categories_id' => 4,
+            'categories_id' => 8,
             'price' => 60000+130000,
             'price3'=>63333
         ]);
         DB::table('products')->insert([
             'name' => 'Cosmos CWD1138',
-            'categories_id' => 4,
+            'categories_id' => 8,
             'price' => 60000+148000,
             'price3'=>69333
         ]);
         DB::table('products')->insert([
             'name' => 'Miyako WD290',
-            'categories_id' => 4,
+            'categories_id' => 8,
             'price' => 60000+304000,
             'price3'=>121333
         ]);
