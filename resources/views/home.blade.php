374030933853b82d36b11fe2a77c593a4afffc9c
fauzifadillah <fauzifadillah00@yahoo.com>
2020-10-25 16:33:33 +0700

    hotjar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -110,7 +110,19 @@
   </div>
 </div>
 {{-- End of Footer --}}
-
+{{-- HotJar Analytics --}}
+<!-- Hotjar Tracking Code for www.temanwarung.id -->
+<script>
+  (function(h,o,t,j,a,r){
+      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+      h._hjSettings={hjid:2059658,hjsv:6};
+      a=o.getElementsByTagName('head')[0];
+      r=o.createElement('script');r.async=1;
+      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+      a.appendChild(r);
+  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+{{-- End of HotJar Analytics --}}
 <!-- jQuery -->
 <script src="{{ asset('js/jquery-3.5.1.js') }}"></script>
 <!-- Bootstrap -->
